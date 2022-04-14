5fbaf2ad783c60a8aafa496010f7e0bc8b4ff04a
MargretMathew <margretmathew55@gmail.com>
2022-04-14 15:53:12 -0500

    uploaded

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa4mtg6z1suNZa2h5d7',
+        issuer: 'https://dev-4508098.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
